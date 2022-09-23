9ed7c6fe262b34e4fa96e2ca23218a80b8345c46
Bailey Kuebelbeck <bailey.kue@gmail.com>
2022-09-22 18:18:22 -0700

    set up inital file structure, imported all css files into components, and set up app js

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,16 @@
+import Layout from './components/Layout/Layout';
+import About from './components/About/About';
+import Projects from './components/Projects/Projects';
+import Resume from './components/Resume/Resume';
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div>
+      <Layout>
+        <About />
+        <Projects />
+        <Resume />
+      </Layout>
+    </div>
+  );
 }
