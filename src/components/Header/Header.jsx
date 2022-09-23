d80fb060032e8100bf92d1307db343e582fd5c0e
Bailey Kuebelbeck <bailey.kue@gmail.com>
2022-09-23 11:45:33 -0700

    added basic styling layout to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,18 @@
-import './Header.css';
+import styles from './Header.css';
+import logo from '../../assets/logo.png';
 
 export default function Header() {
-  return <div>Header</div>;
+  const { header, bk_logo, menu } = styles;
+  return (
+    <header className={header}>
+      <div className={bk_logo}>
+        <img src={logo} />
+      </div>
+      <div className={menu}>
+        <a>About</a>
+        <a>Projects</a>
+        <a>Resume</a>
+      </div>
+    </header>
+  );
 }
