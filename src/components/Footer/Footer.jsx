eb8db1e0b76a7b6f9d904d50665a1ca9cfca3463
Bailey Kuebelbeck <bailey.kue@gmail.com>
2022-09-23 13:11:51 -0700

    adding same style to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,12 @@
-import './Footer.css';
+import styles from './Footer.css';
 
 export default function Footer() {
-  return <div>Footer</div>;
+  const { footer } = styles;
+  return (
+    <footer className={footer}>
+      <a href="https://www.linkedin.com/in/bailey-kuebelbeck/">LinkedIn</a>
+      <a href="https://github.com/baileykue">Github</a>
+      <a>Email</a>
+    </footer>
+  );
 }
