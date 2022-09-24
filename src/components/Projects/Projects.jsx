d9a96c0a2f10378eeb3fdd45bf170eddd37b62d8
Bailey Kuebelbeck <bailey.kue@gmail.com>
2022-09-23 17:47:07 -0700

    starting to add info to portfolio section, trying to add favicon

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -4,7 +4,16 @@ export default function Projects() {
   const { projects } = styles;
   return (
     <div id="projects-id" className={projects}>
-      Projects
+      <h1>Projects</h1>
+      <div>
+        <h2>Resourcery</h2>
+      </div>
+      <div>
+        <h2>friends.map()</h2>
+      </div>
+      <div>
+        <h2>Scrounger</h2>
+      </div>
     </div>
   );
 }
